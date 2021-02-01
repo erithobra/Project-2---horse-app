aa5dacec28f6285585d104701a200430e623274d
ericb2020 <ebrauer83@gmail.com>
2021-02-01 17:53:35 -0600

    push to start collaboration

---
+++ controllers/horses.js
@@ -0,0 +1,8 @@
+const Horse = require("../models").Horse;
+
+const show = (req, res) => {
+// app.get("/horses/:index", function(req, res) {
+//     res.render("show.ejs", {
+//         horse: horses[req.params.index]
+//     });
+// });
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require ('express');
+const methodOverride = require('method-override');
+const routes = require('./routes');
 const app = express(); // app is an object
 
 const horses = require('./models/horses.js');
+
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.use(express.static("public"));
+
+
+app.post("/horses", (req, res) => {
+    res.send("hi");
+});
+
 
 
 app.get("/horses/", (req, res) => {
     res.send(horses);
 });
 
-app.get("/horses/:id", function(req, res) {
-    res.render("show.ejs", {
-        horse: horses[req.params.id]
-    });
-});
+// app.get("/horses/:index", function(req, res) {
+//     res.render("show.ejs", {
+//         horse: horses[req.params.index]
+//     });
+// });
 
 app.listen(3000, () => {
     console.log("I am listening");
