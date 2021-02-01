f3768959f8026f04eddad3a535d19fcc60698271
ericb2020 <ebrauer83@gmail.com>
2021-02-01 17:07:01 -0600

    additional sever setup

---
+++ models/horses.js
@@ -0,0 +1,16 @@
+const horses = [
+    {
+        name:'horse1',
+        color: 'red',
+    },
+    {
+        name:'horse2',
+        color: 'green',
+    },
+    {
+        name:'horse3',
+        color: 'yellow',
+    }
+];
+
+module.exports = horses;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require ('express');
 const app = express(); // app is an object
 
-app.get('/somedata', (req, res) => {
-    res.send('here is your information');
+const horses = require('./models/horses.js');
+
+
+app.get("/horses/", (req, res) => {
+    res.send(horses);
+});
+
+app.get("/horses/:id", function(req, res) {
+    res.render("show.ejs", {
+        horse: horses[req.params.id]
+    });
 });
 
 app.listen(3000, () => {
