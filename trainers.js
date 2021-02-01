4b822606adb4f058702f84716315e353ab8e1273
ericb2020 <ebrauer83@gmail.com>
2021-02-01 17:22:38 -0600

    push to start collaboration

---
+++ trainers.js
@@ -0,0 +1,19 @@
+const trainers = [
+    {
+        name:'trainer1',
+        username: 'trainer1',
+        password: 'trainer1'
+    },
+    {
+        name:'trainer2',
+        username: 'trainer2',
+        password: `trainer2`
+    },
+    {
+        name:'trainer3',
+        username: 'trainer3',
+        password: 'trainer3'
+    }
+]
+
+module.exports = trainers;
