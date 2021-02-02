52664f0dd54fc8f99e53d13b814f198bbde10752
wtasch <wtasch@aol.com>
2021-02-01 20:08:59 -0500

    set up and populated Horses table in database

--- migrations/20210202002028-create-horse.js
+++ migrations/20210202002028-create-horse.js
@@ -49,11 +49,14 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        defaultValue: new Date(),
         type: Sequelize.DATE
+        
       }
     });
   },
